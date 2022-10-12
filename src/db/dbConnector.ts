88d28345692079565e3c2c1935e7e1f1d3dd5e49
kumar-neeraj-001 <neeraj@flick2know.com>
2022-10-12 22:43:09 +0530

    connect hostinger mysql db

--- src/db/dbConnector.ts
+++ src/db/dbConnector.ts
@@ -1,14 +1,18 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestAccounts } from './entities/test_accounts.entity';
 import { TestTransactions } from './entities/test_transaction.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = Number(process.env.PORT);
 
 export const typeOrmModule = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Neeraj@123',
-  database: 'sql_hr',
+  host: process.env.HOST,
+  port: PORT,
+  username: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   entities: [TestAccounts, TestTransactions],
   synchronize: false,
   logging: false,
