2a83d17e23cd2d460c30efb4bd451eee01cd6dc3
kumar-neeraj-001 <neeraj@flick2know.com>
2022-10-13 19:40:55 +0530

    dev: add new origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://finance-chi.vercel.app'],
     credentials: true,
   });
   await app.listen(PORT);
